8b6b81428111239e62240a487832f32cc5d09120
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-10-15 14:36:11 +0300

    Final commit.

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,33 +1,28 @@
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from "../features/user/userSlice.js";
+} from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
-import OAuth from "../components/OAuth.jsx";
+import OAuth from "../components/OAuth";
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
-
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch("/api/auth/sign-in", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -35,19 +30,16 @@ const SignIn = () => {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-
       if (data.success === false) {
         dispatch(signInFailure(data));
         return;
       }
-
       dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
       dispatch(signInFailure(error));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -75,18 +67,14 @@ const SignIn = () => {
         <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Don&apos;t have an account?</p>
+        <p>Don&apos;t Have an account?</p>
         <Link to="/sign-up">
-          <span className="text-blue-500">Sign Up</span>
+          <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <div>
-        <p className="text-red-600 mt-5">
-          {error ? error.message || "Something Went Wrong!" : ""}
-        </p>
-      </div>
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Something went wrong!" : ""}
+      </p>
     </div>
   );
-};
-
-export default SignIn;
+}
